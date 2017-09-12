0e64a95e5a0da881b31d50377b6244f5f28a534a
rezakov <nikolay.petkov@gmail.com>
2017-09-12 15:50:08 -0400

    add edit-title element

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia with Redux';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./views/welcome'),      nav: true, title: 'Welcome' }
+      { route: ['', 'welcome'], name: 'welcome', moduleId: PLATFORM.moduleName('./views/welcome'), nav: true, title: 'Welcome' }
     ]);
 
     this.router = router;
--- src/views/welcome.js
+++ src/views/welcome.js
@@ -1,4 +1,25 @@
+import { inject, observable } from 'aurelia-framework';
+
 
 export class Welcome {
 
+  count = 0;//# of times title was modified
+
+  @observable()
+  title = 'Meetup at Ellevation Education';
+
+  constructor() {
+    this.people = [
+      { fname: 'Tom', lname: 'Smith' },
+      { fname: 'Mark', lname: 'Doe' }
+    ];
+  }
+
+  titleChanged() {
+    this.count ++;
+  }
+
+  toggleEditElement() {
+    this.showEditTitleElement = !this.showEditTitleElement;
+  }
 }
